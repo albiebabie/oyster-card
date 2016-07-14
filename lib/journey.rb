e773424c1d9f50f072a79fd0cd896a816ef23254
6eff <6eff@mail.ru>
2016-07-14 15:42:42 +0300

    completes? journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,20 +1,26 @@
 class Journey
 
-  attr_reader :in_journey
+  attr_reader :journey
 
   def initialize
-    @in_journey = false
+    @journey = {}
   end
 
-  def start_journey
+  def start_journey entry_station
     @in_journey = true
+    @journey[:start] = entry_station
   end
 
-  def end_journey
+  def end_journey exit_station
     @in_journey = false
+    @journey[:end]  = exit_station
   end
 
   def in_journey?
     @in_journey
+  end
+
+  def complete?
+    !@journey[:start].nil? && !@journey[:end].nil?
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,14 +1,28 @@
 require 'journey'
 
 describe Journey do
-
-  it 'starts journey' do
-    subject.start_journey
+let(:entry_station) {double :entry_station}
+let(:exit_station) {double :exit_station}
+  it '#start_journey' do
+    subject.start_journey entry_station
     expect(subject).to be_in_journey
   end
-  it 'ends journey' do
-    subject.start_journey
-    subject.end_journey
+
+  it '#end_journey' do
+    subject.start_journey entry_station
+    subject.end_journey exit_station
     expect(subject).not_to be_in_journey
   end
+
+  it 'returns complete? for the journey' do
+    subject.start_journey entry_station
+    expect(subject).not_to be_complete
+  end
+
+  it 'calculates fare' do
+    subject.start_journey entry_station
+    subject.end_journey exit_station
+    expect(subject.fare).to eq 1
+  end
+
 end
