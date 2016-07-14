7298d54e9bc63a6fe297c04f422cee070426e809
albiebabie <albiebabie@gmail.com>
2016-07-14 16:04:33 +0100

    challenge 14 adds journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,19 +1,22 @@
 class Journey
 
+MINIMUM_FARE = 1
+PENALTY_FARE = 6
+
   attr_reader :journey
 
   def initialize
     @journey = {}
   end
 
-  def start_journey entry_station
+  def start_journey(entry_station)
     @in_journey = true
     @journey[:start] = entry_station
   end
 
-  def end_journey exit_station
+  def end_journey(exit_station)
     @in_journey = false
-    @journey[:end]  = exit_station
+    @journey[:end] = exit_station
   end
 
   def in_journey?
@@ -22,5 +25,13 @@ class Journey
 
   def complete?
     !@journey[:start].nil? && !@journey[:end].nil?
+  end
+
+  def fare
+    if complete?
+      MINIMUM_FARE
+    else
+      PENALTY_FARE
+    end
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,30 +2,44 @@ require 'journey'
 
 
 describe Journey do
-let(:entry_station) {double :entry_station}
-let(:exit_station) {double :exit_station}
-  it '#start_journey' do
-    subject.start_journey entry_station
-    expect(subject).to be_in_journey
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
+
+    it '#start_journey' do
+      subject.start_journey entry_station
+      expect(subject).to be_in_journey
+    end
+
+
+  describe '#end_journey' do
+    it 'changes in_journey to true' do
+      subject.start_journey entry_station
+
+      subject.end_journey exit_station
+      expect(subject).not_to be_in_journey
+    end
+
+
+    it 'returns complete? for the journey' do
+      subject.start_journey entry_station
+      expect(subject).not_to be_complete
+    end
   end
 
-  it '#end_journey' do
-    subject.start_journey entry_station
-
-    subject.end_journey exit_station
-    expect(subject).not_to be_in_journey
-  end
-
-
-  it 'returns complete? for the journey' do
-    subject.start_journey entry_station
-    expect(subject).not_to be_complete
-  end
-
-  it 'calculates fare' do
-    subject.start_journey entry_station
-    subject.end_journey exit_station
-    expect(subject.fare).to eq 1
+  describe "#fare" do
+    it 'charges minimum fare' do
+      subject.start_journey entry_station
+      subject.end_journey exit_station
+      expect(subject.fare).to eq described_class::MINIMUM_FARE
+    end
+    it 'charges penalty fare if not touched out' do
+      subject.start_journey entry_station
+      expect(subject.fare).to eq described_class::PENALTY_FARE
+    end
+    it 'charges penalty fare if not touched in' do
+      subject.end_journey exit_station
+      expect(subject.fare).to eq described_class::PENALTY_FARE
+    end
   end
 
 end
